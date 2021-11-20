adf092ca5b373629260ef44dd606e0cc8879e50b
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2021-11-20 16:36:58 -0600

    Initial Commit

---
+++ src/main/PlaceholderTextField.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.JTextField;
+
+/**
+ * Simple class that displays a placeholder on the text field 
+ * @author Edgar Trejo Avila
+ *
+ */
+@SuppressWarnings("serial")
+public class PlaceholderTextField extends JTextField {
+	String placeholder;
+	
+	
+	
+}
